edb96643779458a23f7a73f02051c96625ca8b31
unknown <Guilherme_dev@1213532-P.asav.brm>
2020-07-23 10:31:18 -0300

    Realocate to scene

---
+++ Assets/UIElements/Scripts/Interactions/RealocateToScene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RealocateToScene : MonoBehaviour
+{
+    public MeshRenderer meshLimits;
+
+    private Vector3 initialPosition;
+    private Vector3 limitPosition;
+    private const int waitSeconds = 2;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+
+        //Setando a posição limite
+        limitPosition = meshLimits.bounds.extents;
+
+        StartCoroutine(VerifyObject(waitSeconds));
+    }
+
+    IEnumerator VerifyObject(int waitSeconds)
+    {
+        if (surpassLimits())
+        {
+            transform.position = initialPosition;
+        }
+
+        yield return new WaitForSeconds(waitSeconds);
+
+
+        StartCoroutine(VerifyObject(waitSeconds));
+    }
+
+    bool surpassLimits()
+    {
+        //Sempre considerar o offset em relação ao mundo
+        return (transform.position.x > (limitPosition.x + meshLimits.transform.position.x) ||
+            transform.position.x < (-limitPosition.x + meshLimits.transform.position.x) ||
+            transform.position.y > (limitPosition.y + meshLimits.transform.position.y) ||
+            transform.position.y < (-limitPosition.y + meshLimits.transform.position.y) ||
+            transform.position.z > (limitPosition.x + meshLimits.transform.position.z) ||
+            transform.position.z < (-limitPosition.x + meshLimits.transform.position.z));
+    }
+}
