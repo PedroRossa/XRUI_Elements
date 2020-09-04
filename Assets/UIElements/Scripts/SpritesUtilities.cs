9c1d7499af3b4174904afd9e0898fd0bb22393b2
unknown <Guilherme_dev@1213532-P.asav.brm>
2020-09-04 14:30:20 -0300

    Adding SpritesUtilities script

---
+++ Assets/UIElements/Scripts/SpritesUtilities.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SpritesUtilities : MonoBehaviour
+{
+    public Sprite[] sprites;
+
+    private XRUI_3DButtonSprite Button_mainSprite;
+    private int index = 0;
+
+    private void Start()
+    {
+        Button_mainSprite = gameObject.GetComponent<XRUI_3DButtonSprite>();
+    }
+    public void changeSprite()
+    {
+        if (Button_mainSprite.canActiveButton)
+        {
+            Button_mainSprite.iconSprite.sprite = sprites[index];
+            index = (index + 1) % sprites.Length;
+        }
+    }
+}
